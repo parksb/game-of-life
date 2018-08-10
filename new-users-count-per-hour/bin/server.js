632bfd11a1d22da43f51b741ffd2ac63e0f5b171
Troy Tae <tjy9745@gmail.com>
2018-08-10 16:36:22 +0900

    그래프 폴더 분리

--- new-users-count-per-hour/bin/server.js
+++ new-users-count-per-hour/bin/server.js
@@ -1,6 +1,10 @@
-var express = require('express');
-var router = express.Router();
+var router = require('express').Router();
+var path = require('path');
 var database = require('./database');
+
+router.get('/graph', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/graph/graph.html'));
+});
 
 router.get('/user-count', function(req, res) {
     console.log(database.selectByDate(0, Infinity).length);
