d786ebc27c7f12452f6af9b82e16d2995b8b241a
Troy Tae <tjy9745@gmail.com>
2018-08-10 16:18:30 +0900

    데이터 분류 알고리즘 개선

--- new-users-count-per-hour/public/graph.js
+++ new-users-count-per-hour/public/graph.js
@@ -1,89 +1,102 @@
 (function () {
-    let svg = d3.select('#graph'),
-        margin = {
+    // svg 관련 정보
+    let graph = {
+        svg: d3.select('#graph'),
+        g: d3.select('#graph').append('g'),
+        line: (d3.line()
+            .curve(d3.curveBasis)
+            .x(d => position.x(d.date))
+            .y(d => position.y(d.usersCount))
+        ),
+        margin: {
             top: 20,
             right: 80,
             bottom: 30,
             left: 50
         },
-        width = svg.attr('width') - margin.left - margin.right,
-        height = svg.attr('height') - margin.top - margin.bottom,
-        g = svg.append('g').attr('transform', `translate(${margin.left},${margin.top})`)
-    ;
-    let x = d3.scaleTime().range([0, width]),
-        y = d3.scaleLinear().range([height, 0]),
-        z = d3.scaleOrdinal(d3.schemeCategory10)
-    ;
-    let line = d3.line()
-        .curve(d3.curveBasis)
-        .x(function(d) { return x(d.date); })
-        .y(function(d) { return y(d.usersCount); })
-    ;
+        get width() { return (this.svg.attr('width') - this.margin.left - this.margin.right); } ,
+        get height() { return (this.svg.attr('height') - this.margin.top - this.margin.bottom); },
+    };
+    // 상대 위치 계산
+    let position = {
+        x: d3.scaleTime().range([0, graph.width]),
+        y: d3.scaleLinear().range([graph.height, 0]),
+        z: d3.scaleOrdinal(d3.schemeCategory10)
+    };
 
     let http = new XMLHttpRequest();
     http.addEventListener('load', function () {
-        let data = JSON.parse(http.responseText);
-        let cities = [];
+        const ONE_HOUR = 3600000;
+        let data = JSON.parse(http.responseText),
+            platforms = {}
+        ;
 
-        for(let i = 0, max = data.length, standardTime = data[0] && data[0].date + 3600; i < max; ++i) {
+        /* 시간대 별 데이터 분류 */
+        for (let i = 0, max = data.length, standardTime = data[i] && data[i].date + ONE_HOUR; i < max; ++i) {
             let v = data[i];
-            cities.some(city => city.id === v.device) || cities.push({
-                id: v.device,
-                values: []
-            });
-
-            (standardTime < v.date) && (standardTime += 3600000);
-
-            let city = cities.filter(city => city.id === v.device)[0];
-            city.values.some(v => v.date.getTime() === standardTime) || city.values.push({
-                date: new Date(standardTime),
-                usersCount: 0
-            });
-            ++city.values.filter(v => v.date.getTime() === standardTime)[0].usersCount;
+            platforms[v.device] || (platforms[v.device] = {});
+            (standardTime < v.date) && (standardTime += ONE_HOUR);
+            let platform = platforms[v.device];
+            platform[standardTime] ? ++platform[standardTime] : (platform[standardTime] = 1);
         }
-        x.domain(d3.extent(data, function(d) { return d.date; }));
 
-        y.domain([
-            d3.min(cities, function(c) { return d3.min(c.values, function(d) { return d.usersCount; }); }),
-            d3.max(cities, function(c) { return d3.max(c.values, function(d) { return d.usersCount; }); })
+        // D3 데이터 형식으로 변환
+        platforms = Object.keys(platforms).map(k => {
+            let platform = platforms[k];
+            return {
+                id: k,
+                values: Object.keys(platform).map(time => ({
+                    date: new Date(parseInt(time)),
+                    usersCount: platform[time]
+                }))
+            };
+        });
+
+        position.x.domain(d3.extent(data, d => d.date));
+        position.y.domain([
+            d3.min(platforms, c => d3.min(c.values, d => d.usersCount)),
+            d3.max(platforms, c => d3.max(c.values, d => d.usersCount))
         ]);
+        position.z.domain(platforms.map(c => c.id));
 
-        z.domain(cities.map(function(c) { return c.id; }));
-
-        g.append('g')
-            .attr('class', 'axis axis--x')
-            .attr('transform', 'translate(0,' + height + ')')
-            .call(d3.axisBottom(x));
-
-        g.append('g')
-            .attr('class', 'axis axis--y')
-            .call(d3.axisLeft(y))
+        graph.g
+            .attr('transform', `translate(${graph.margin.left},${graph.margin.top})`)
+            .append('g')
+            .attr('transform', `translate(0,${graph.height})`)
+            .call(d3.axisBottom(position.x))
+        ;
+        graph.g.append('g').call(d3.axisLeft(position.y))
             .append('text')
             .attr('transform', 'rotate(-90)')
             .attr('y', 6)
-            .attr('dy', '0.71em')
+            .attr('dy', '0.7em')
             .attr('fill', '#000')
-            .text('usersCount, ºF');
+            .style('font', '12px sans-serif')
+            .text('사용자 수')
+        ;
 
-        let city = g.selectAll('.city')
-            .data(cities)
-            .enter().append('g')
-            .attr('class', 'city');
-
-        city.append('path')
-            .attr('d', function(d) { return line(d.values); })
+        let platform = graph.g.selectAll()
+            .data(platforms)
+            .enter()
+            .append('g')
+        ;
+        platform.append('path')
+            .attr('d', d => graph.line(d.values))
             .style('fill', 'none')
-            .style('stroke', 'steelblue')
             .style('stroke-width', '1.5px')
-            .style('stroke', function(d) { return z(d.id); });
-
-        city.append('text')
-            .datum(function(d) { return {id: d.id, value: d.values[d.values.length - 1]}; })
-            .attr('transform', function(d) { return 'translate(' + x(d.value.date) + ',' + y(d.value.usersCount) + ')'; })
+            .style('stroke', d => position.z(d.id))
+        ;
+        platform.append('text')
+            .datum(d => ({
+                id: d.id,
+                value: d.values[d.values.length - 1]
+            }))
+            .attr('transform', d => `translate(${position.x(d.value.date)},${position.y(d.value.usersCount)})`)
             .attr('x', 3)
-            .attr('dy', '0.35em')
-            .style('font', '10px sans-serif')
-            .text(function(d) { return d.id; });
+            .attr('dx', '-0.1em')
+            .style('font', '12px sans-serif')
+            .text(d => d.id)
+        ;
     });
     http.open('GET', 'user-count');
     http.send();
