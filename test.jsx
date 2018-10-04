adec442c497fce1a4ab3189820c1fd112a952c09
Troy Tae <tjy9745@gmail.com>
2018-10-04 12:24:25 +0900

    First React App

---
+++ test.jsx
@@ -0,0 +1,22 @@
+'use strict';
+
+class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
+    }
+
+    render() {
+        if (this.state.liked) {
+            return 'You liked this.';
+        }
+
+        return (
+            <button onClick={() => this.setState({ liked: true }) }>
+                Like
+            </button>
+        );
+    }
+}
+
+ReactDOM.render(<LikeButton />, document.getElementById('wrap'));
