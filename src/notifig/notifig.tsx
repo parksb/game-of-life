305bf431485282f82ec9b953eadd9ae03169e375
Troy Tae <tjy9745@gmail.com>
2018-10-29 16:00:22 +0900

    import checkbox

--- src/notifig/notifig.tsx
+++ src/notifig/notifig.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Checkbox from '@material/react-checkbox';
 
 const styles = require('./notifig.scss');
 
@@ -8,6 +9,18 @@ export class Notifig extends React.Component {
     }
 
     render() {
-        return <h1>Notifig</h1>;
+        return <div id={styles.notice_config} className="wrap">
+            <h3>광고 알림 메시지 수신 설정</h3>
+            <p>광고운영 및 검수와 관련된 메시지 수신 여부를 설정합니다.</p>
+            <Checkbox
+                nativeControlId='my-checkbox'
+                checked={true}
+                indeterminate={false}
+                onChange={(e) => this.setState({
+                    checked: e.target.checked,
+                    indeterminate: e.target.indeterminate})
+                }
+            />
+        </div>;
     }
 }
