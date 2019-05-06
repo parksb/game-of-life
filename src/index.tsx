f4b8c15ec2b2b3a4a5b3898e40c7f7fa021fa0c4
TroyTae <tjy970721@gmail.com>
2019-05-07 00:53:47 +0900

    indent to spaces

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import {Component} from 'inferno';
 
 export default class MyComponent extends Component {
-	render() {
-		return (
-			<div>
-				<h1>Header!</h1>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <h1>Header!</h1>
+      </div>
+    );
+  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,31 +3,31 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const transformInferno = require('ts-transform-inferno').default;
 
 module.exports = {
-	entry: "./src/index.tsx",
-	output: {
-		path: path.join(process.cwd(), "dist"),
-		libraryTarget: "umd",
-		filename: "[name].js"
-	},
-	externals: ["inferno"],
-	resolve: {
-		extensions: [".js", ".jsx", ".ts", ".tsx"]
-	},
-	module: {
-		rules: [{
-			test: /\.tsx$/,
-			loader: 'ts-loader',
-			options: {
-				getCustomTransformers: () => ({
-					before: [transformInferno()],
-				}),
-			},
-		}, {
-			test: /\.ts$/,
-			loader: 'ts-loader',
-		}]
-	},
-	plugins: [
-		new CleanWebpackPlugin({})
-	],
+  entry: "./src/index.tsx",
+  output: {
+    path: path.join(process.cwd(), "dist"),
+    libraryTarget: "umd",
+    filename: "[name].js"
+  },
+  externals: ["inferno"],
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"]
+  },
+  module: {
+    rules: [{
+      test: /\.tsx$/,
+      loader: 'ts-loader',
+      options: {
+        getCustomTransformers: () => ({
+          before: [transformInferno()],
+        }),
+      },
+    }, {
+      test: /\.ts$/,
+      loader: 'ts-loader',
+    }]
+  },
+  plugins: [
+    new CleanWebpackPlugin({})
+  ],
 };
