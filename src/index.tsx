64fb8a38d9e86a1003b6bed1ba103ad3b036ac2d
Troy Tae <tjy970721@gmail.com>
2019-05-14 16:51:16 +0900

    make checkbox example

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import MDCButton from '@material/react-button';
+
+import classnames from 'classnames';
+import Button from '@material/react-button';
+import Checkbox from '@material/react-checkbox';
 import MaterialIcon from "@material/react-material-icon";
 
 const styles = require('./index.scss');
@@ -8,14 +11,29 @@ const styles = require('./index.scss');
 const Component = () => <div className={styles.container}>
   <h1>Button</h1>
   <div className={styles.wrapper}>
-    <MDCButton className={styles.component}>default</MDCButton>
-    <MDCButton className={styles.component} raised={true}>raised</MDCButton>
-    <MDCButton className={styles.component} unelevated={true}>unelevated</MDCButton>
-    <MDCButton className={styles.component} outlined={true}>outlined</MDCButton>
-    <MDCButton className={styles.component} dense={true}>dense</MDCButton>
-    <MDCButton className={styles.component} icon={<MaterialIcon icon={'stars'}/>}>icon</MDCButton>
-    <MDCButton className={styles.component} trailingIcon={<MaterialIcon icon={'cached'}/>}>trailing-icon</MDCButton>
-    <MDCButton className={styles.component} disabled={true}>disabled</MDCButton>
+    <Button className={styles.component}>default</Button>
+    <Button className={styles.component} raised={true}>raised</Button>
+    <Button className={styles.component} unelevated={true}>unelevated</Button>
+    <Button className={styles.component} outlined={true}>outlined</Button>
+    <Button className={styles.component} dense={true}>dense</Button>
+    <Button className={styles.component} icon={<MaterialIcon icon={'stars'}/>}>icon</Button>
+    <Button className={styles.component} trailingIcon={<MaterialIcon icon={'cached'}/>}>trailing-icon</Button>
+    <Button className={styles.component} disabled={true}>disabled</Button>
+  </div>
+  <h1>Checkbox</h1>
+  <div className={styles.wrapper}>
+    <div className={classnames([styles.component, styles.checkbox])}>
+      <Checkbox nativeControlId='checkbox1' className={styles.check}/>
+      <label htmlFor='checkbox1' className={styles.label}>Checkbox1</label>
+    </div>
+    <div className={classnames([styles.component, styles.checkbox])}>
+      <Checkbox nativeControlId='checkbox2' className={styles.check} indeterminate={true}/>
+      <label htmlFor='checkbox2' className={styles.label}>Checkbox2</label>
+    </div>
+    <div className={classnames([styles.component, styles.checkbox])}>
+      <Checkbox nativeControlId='checkbox3' className={styles.check} disabled={true}/>
+      <label htmlFor='checkbox3' className={styles.label}>Checkbox3</label>
+    </div>
   </div>
 </div>;
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,41 +2,44 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    path: path.join(process.cwd(), "docs"),
-    filename: "[name].[chunkhash].js",
-    crossOriginLoading: false
-  },
-  resolve: { extensions: [".js", ".jsx", ".ts", ".tsx"] },
-  module: {
-    rules: [{
-      test: /\.scss$/,
-      loader: [
-        "style-loader",
-        {
-          loader: "css-loader",
-          options: {
-            modules: 'global'
+module.exports = (env, arg) => {
+  const isProd = (arg.mode === 'production');
+  return {
+    entry: "./src/index.tsx",
+    output: {
+      path: path.join(process.cwd(), "docs"),
+      filename: "[name].[chunkhash].js",
+      crossOriginLoading: false
+    },
+    resolve: { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    module: {
+      rules: [{
+        test: /\.scss$/,
+        loader: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: 'global'
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: { includePaths: ["./node_modules"] }
           }
-        },
-        {
-          loader: "sass-loader",
-          options: { includePaths: ["./node_modules"] }
-        }
-      ]
-    }, {
-      test: /\.tsx?$/,
-      loader: "ts-loader"
-    }]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({ template: "./src/index.html" })
-  ],
-  devServer: {
-    port: 4200,
-    historyApiFallback: true
-  },
+        ]
+      }, {
+        test: /\.tsx?$/,
+        loader: "ts-loader"
+      }]
+    },
+    plugins: [
+      isProd && new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({ template: "./src/index.html" })
+    ].filter(v => v),
+    devServer: {
+      port: 4200,
+      historyApiFallback: true
+    },
+  };
 };
