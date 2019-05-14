c14018ed7545714fab243b09d828c2ef121022ec
Troy Tae <tjy970721@gmail.com>
2019-05-14 17:22:50 +0900

    make grid

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,41 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import classnames from 'classnames';
 import Button from '@material/react-button';
 import Checkbox from '@material/react-checkbox';
 import MaterialIcon from "@material/react-material-icon";
+import {Cell, Grid, Row} from '@material/react-layout-grid';
+
 
 const styles = require('./index.scss');
 
-const Component = () => <div className={styles.container}>
+const Component = () => <Grid>
   <h1>Button</h1>
-  <div className={styles.wrapper}>
-    <Button className={styles.component}>default</Button>
-    <Button className={styles.component} raised={true}>raised</Button>
-    <Button className={styles.component} unelevated={true}>unelevated</Button>
-    <Button className={styles.component} outlined={true}>outlined</Button>
-    <Button className={styles.component} dense={true}>dense</Button>
-    <Button className={styles.component} icon={<MaterialIcon icon={'stars'}/>}>icon</Button>
-    <Button className={styles.component} trailingIcon={<MaterialIcon icon={'cached'}/>}>trailing-icon</Button>
-    <Button className={styles.component} disabled={true}>disabled</Button>
-  </div>
+  <Row>
+    <Cell><Button>default</Button></Cell>
+    <Cell><Button raised={true}>raised</Button></Cell>
+    <Cell><Button unelevated={true}>unelevated</Button></Cell>
+    <Cell><Button outlined={true}>outlined</Button></Cell>
+    <Cell><Button dense={true}>dense</Button></Cell>
+    <Cell><Button icon={<MaterialIcon icon={'stars'}/>}>icon</Button></Cell>
+    <Cell><Button trailingIcon={<MaterialIcon icon={'cached'}/>}>trailing-icon</Button></Cell>
+    <Cell><Button disabled={true}>disabled</Button></Cell>
+  </Row>
   <h1>Checkbox</h1>
-  <div className={styles.wrapper}>
-    <div className={classnames([styles.component, styles.checkbox])}>
+  <Row>
+    <Cell className={styles.checkbox}>
       <Checkbox nativeControlId='checkbox1' className={styles.check}/>
       <label htmlFor='checkbox1' className={styles.label}>Checkbox1</label>
-    </div>
-    <div className={classnames([styles.component, styles.checkbox])}>
+    </Cell>
+    <Cell className={styles.checkbox}>
       <Checkbox nativeControlId='checkbox2' className={styles.check} indeterminate={true}/>
       <label htmlFor='checkbox2' className={styles.label}>Checkbox2</label>
-    </div>
-    <div className={classnames([styles.component, styles.checkbox])}>
+    </Cell>
+    <Cell className={styles.checkbox}>
       <Checkbox nativeControlId='checkbox3' className={styles.check} disabled={true}/>
       <label htmlFor='checkbox3' className={styles.label}>Checkbox3</label>
-    </div>
-  </div>
-</div>;
+    </Cell>
+  </Row>
+</Grid>;
 
 ReactDOM.render(<Component/>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -3,43 +3,47 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = (env, arg) => {
-  const isProd = (arg.mode === 'production');
-  return {
+  const config = {
     entry: "./src/index.tsx",
     output: {
       path: path.join(process.cwd(), "docs"),
       filename: "[name].[chunkhash].js",
       crossOriginLoading: false
     },
-    resolve: { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    resolve: {
+      extensions: [".js", ".jsx", ".ts", ".tsx"]
+    },
     module: {
       rules: [{
         test: /\.scss$/,
-        loader: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              modules: 'global'
-            }
-          },
-          {
-            loader: "sass-loader",
-            options: { includePaths: ["./node_modules"] }
-          }
-        ]
+        loader: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader",
+          options: { modules: 'global' }
+        }, {
+          loader: "sass-loader",
+          options: { includePaths: ["./node_modules"] }
+        }]
       }, {
         test: /\.tsx?$/,
         loader: "ts-loader"
       }]
     },
     plugins: [
-      isProd && new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({ template: "./src/index.html" })
-    ].filter(v => v),
+    ],
     devServer: {
       port: 4200,
       historyApiFallback: true
     },
   };
+
+  if (arg.mode === 'production') {
+    config.plugins = config.plugins.concat([
+      new CleanWebpackPlugin()
+    ]);
+  }
+
+  return config;
 };
