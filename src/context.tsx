146364156abc3e731242ec98c26316da531bbc94
TroyTae <tjy970721@gmail.com>
2019-06-09 16:27:48 +0900

    delete files

--- src/context.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-
-export const MenuContext = React.createContext({
-  isOpen: false,
-  selectedIndex: 0,
-  setOpen: (open: boolean) => {},
-  setSelectedIndex: (selectedIndex: number) => {},
-});
