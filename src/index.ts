9f96a2955fca1d555b9626d5d2a80f9dff413341
ParkSB <parkgds@gmail.com>
2019-10-07 11:19:15 +0900

    Add pulsar pattern

--- src/index.ts
+++ src/index.ts
@@ -35,3 +35,23 @@ render('Toad', new GameOfLifeEngine([
   [false, false, false, false, false, false],
   [false, false, false, false, false, false],
 ]));
+
+render('Pulsar', new GameOfLifeEngine([
+  [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, true,  true,  true,  false, false, false, true,  true,  true,  false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+  [false, false, true,  false, false, false, false, true,  false, true,  false, false, false, false, true,  false, false],
+  [false, false, true,  false, false, false, false, true,  false, true,  false, false, false, false, true,  false, false],
+  [false, false, true,  false, false, false, false, true,  false, true,  false, false, false, false, true,  false, false],
+  [false, false, false, false, true,  true,  true,  false, false, false, true,  true,  true,  false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, true,  true,  true,  false, false, false, true,  true,  true,  false, false, false, false],
+  [false, false, true,  false, false, false, false, true,  false, true,  false, false, false, false, true,  false, false],
+  [false, false, true,  false, false, false, false, true,  false, true,  false, false, false, false, true,  false, false],
+  [false, false, true,  false, false, false, false, true,  false, true,  false, false, false, false, true,  false, false],
+  [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, true,  true,  true,  false, false, false, true,  true,  true,  false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+  [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+]));
